debc1d8ef17ad4c8d4796d689662b0ccd527dc44
SlashArash <slasharash@protonmail.com>
2017-04-17 12:10:13 +0430

    Dashatize kata with php

---
+++ dashatize_it.php
@@ -0,0 +1,9 @@
+<?php
+function dashatize(int $num): string {
+  $num = array_map(function($val) {
+      if($val & 1)
+        return "-$val-";
+      return $val;
+  }, str_split(strval(abs($num))));
+  return str_replace('--', '-', trim(implode($num), '-'));
+}
