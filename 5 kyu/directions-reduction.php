1102e02d15fffc9c824749507a938238e9b42976
SlashArash <slasharash@protonmail.com>
2017-05-02 17:27:54 +0430

    solution for *directions reduction* Kata

---
+++ 5 kyu/directions-reduction.php
@@ -0,0 +1,15 @@
+<?php
+// https://www.codewars.com/kata/directions-reduction
+
+function dirReduc(array $arr): array {
+    $opposite = ["NORTH-SOUTH", "SOUTH-NORTH", "WEST-EAST", "EAST-WEST"];
+    $string = implode("-", $arr);
+    for($i=0; $i<count($arr)/2; $i++) {
+        $string = trim(str_replace($opposite, "", $string), "-");
+        $string = str_replace("--", "-", $string);
+    }
+    if (strlen($string) == 0) return [];
+    return explode("-", $string);
+}
+$a = ["NORTH", "SOUTH","SOUTH", "EAST", "WEST", "NORTH", "WEST"];
+print_r(dirReduc($a));
